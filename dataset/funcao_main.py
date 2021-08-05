c68e07d13a067c90fd7847607b92cbbf42bc02d8
tati <tatiane_correia_19@yahoo.com.br>
2021-08-04 21:14:00 -0300

    funcao de para

--- dataset/funcao_main.py
+++
@@ -1,3 +0,0 @@
-## O objetivo dessa função é copiar o arquivo é ler um arquivo e fazer o output
-
-Class 
---
+++ dataset/teste.py
@@ -0,0 +1,10 @@
+class funcao_main():
+    def __init__(self):
+        self.de = 'from'
+        self.para = 'to'
+
+    def retorna(self):
+        print('caminho: ', self.de)
+
+chama_funcao = funcao_main()
+chama_funcao.retorna()
